3841826e2ae028179086dcefae800a9f2caf5fd6
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-07-24 15:19:22 +0200

    added simple task generation.

--- lib/headcalculation.dart
+++ lib/headcalculation.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Headcalculation extends StatefulWidget {
@@ -7,20 +9,46 @@ class Headcalculation extends StatefulWidget {
 }
 
 class _HeadcalculationState extends State<Headcalculation> {
-  var task = "1 + 2 = ?";
-  var score = 0;
+  _HeadcalculationState() {
+    generateTask();
+  }
 
+  var _task;
+  var _score = 0;
+  var _random = new Random();
+  var _rightResult;
+  var _inputController = TextEditingController();
+
+  void generateTask() {
+    var a = _random.nextInt(100);
+    var b = _random.nextInt(100);
+
+    _task = '$a + $b = ?';
+    _rightResult = a + b;
+  }
+
+  void checkInput() {
+    if (int.parse(_inputController.text) == _rightResult) {
+      generateTask();
+      setState(() {
+        _score = _score + 1;
+      });
+    }
+    _inputController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar( title: const Text("Headcalculation"),),
       body: ListView( children: [
-        ListTile(title: Text(task),),
+        ListTile(title: Text(_task),),
         TextField(
           keyboardType: TextInputType.number,
+          onSubmitted: (value) => checkInput(),
+          controller: _inputController,
         ),
-        ListTile(title: Text('Score: $score'),)
+        ListTile(title: Text('Score: $_score'),)
       ],),
     );
   }
