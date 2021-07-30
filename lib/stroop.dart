2b806fbc499de76532e5f8f6d993c7c9e26b4e50
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-07-30 23:57:47 +0200

    added buttons.

--- lib/stroop.dart
+++ lib/stroop.dart
@@ -12,13 +12,23 @@ class Stroop extends StatefulWidget {
 const colors = {
   'Red': Colors.red,
   'Green': Colors.green,
-  'Blue': Colors.blue
+  'Blue': Colors.blue,
+  'Yellow': Colors.yellow,
+  'Purple': Colors.purple,
+  'Orange': Colors.orange,
+  'Brown': Colors.brown,
+  'Pink': Colors.pink,
+  'Black': Colors.black,
 };
 
 class _StroopState extends State<Stroop> {
   var _color;
   var _colorText;
   var _random = new Random();
+  var _score = 0;
+
+  var _colorTexts = colors.keys;
+  var _colors = colors.values;
 
   _StroopState() {
     generateColor();
@@ -34,9 +44,30 @@ class _StroopState extends State<Stroop> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Stroop"),),
-      body: ListView(
-        children: [
-          ListTile(title: Text(_colorText, style: TextStyle(color: _color),),)
+      body: Column(
+       children: [
+          const Text("data"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            ElevatedButton(child: Text("data"), onPressed: () => {},),
+            ElevatedButton(child: Text("data"), onPressed: () => {},),
+            ElevatedButton(child: Text("data"), onPressed: () => {},)
+          ],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            ElevatedButton(child: Text("data"), onPressed: () => {},),
+            ElevatedButton(child: Text("data"), onPressed: () => {},),
+            ElevatedButton(child: Text("data"), onPressed: () => {},)
+          ],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            ElevatedButton(child: Text("data"), onPressed: () => {},),
+            ElevatedButton(child: Text("data"), onPressed: () => {},),
+            ElevatedButton(child: Text("data"), onPressed: () => {},)
+          ],)
         ],
       ),
     );
