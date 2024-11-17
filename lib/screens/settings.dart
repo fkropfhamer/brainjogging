a0051a93d6846c0bb06b0e1eedd310accd5f1fd8
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2024-11-17 20:28:13 +0100

    add version

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -10,7 +10,7 @@ class Settings extends StatelessWidget {
         title: const Text("Settings"),
         actions: [
           IconButton(
-              onPressed: () => showAboutDialog(context: context),
+              onPressed: () => showAboutDialog(context: context, applicationVersion: "v0.1.0"),
               icon: Icon(Icons.info))
         ],
       ),
